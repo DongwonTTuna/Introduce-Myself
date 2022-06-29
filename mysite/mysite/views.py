1493f05797572d0872052ffde875d295aa7f4442
dongwon <ttuna0790@gmail.com>
2022-06-29 01:30:25 -0700

    updated

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def main(request):
   return render(request, 'main.html')
 def shift(request):
-  return render(request, 'main.html')
+  return render(request, 'sub1.html')
 def kimchi(request):
-  return render(request, 'main.html')
+  return render(request, 'sub2.html')
